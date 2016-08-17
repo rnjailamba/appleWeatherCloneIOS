12bd5370d39fbd910de9e97d1e5a94bc3e044a5b
rnjai <rnjailamba12@gmail.com>
2016-08-17 10:24:07 +0530

    add pagecontent view controller

---
+++ appleWeatherClone/PageContentViewController.h
@@ -0,0 +1,17 @@
+//
+//  PageContentViewController.h
+//  appleWeatherClone
+//
+//  Created by Mr Ruby on 17/08/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageContentViewController : UIViewController
+
+@property NSUInteger pageIndex;
+@property NSString *titleText;
+@property NSString *imageFile;
+
+@end
---
+++ appleWeatherClone/PageContentViewController.m
@@ -0,0 +1,43 @@
+//
+//  PageContentViewController.m
+//  appleWeatherClone
+//
+//  Created by Mr Ruby on 17/08/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import "PageContentViewController.h"
+
+@interface PageContentViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
+@implementation PageContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.view.frame = [[UIScreen mainScreen]bounds];
+    self.imageView.image = [UIImage imageNamed:self.imageFile];
+    self.label.text = self.titleText;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
