c3e2282fc171fee9574e972be6ce7ce7e203414e
rnjai <rnjailamba12@gmail.com>
2016-08-19 09:09:36 +0530

    showing of current address is done other than centering it

--- appleWeatherClone/CurrentViewController.m
+++ appleWeatherClone/CurrentViewController.m
@@ -12,11 +12,12 @@
 
 @interface CurrentViewController ()<CLLocationManagerDelegate>
 // Instantiate a pair of UILabels in Interface Builder
-@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property ( nonatomic)  UILabel *nameLabel;
+@property ( nonatomic)  UILabel *addressLabel;
 @property (weak, nonatomic) IBOutlet UIButton *backButton;
 - (IBAction)backClicked:(id)sender;
 @property (strong, nonatomic) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet UILabel *yourCurrentLocationLabel;
 
 
 @end
@@ -27,32 +28,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.nameLabel sizeToFit];
     _placesClient = [GMSPlacesClient sharedClient];
 
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
-    // Check for iOS 8. Without this guard the code will crash with "unknown selector" on iOS 7.
+    
     if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
         [self.locationManager requestWhenInUseAuthorization];
     }
     [self.locationManager startUpdatingLocation];
     [_placesClient currentPlaceWithCallback:^(GMSPlaceLikelihoodList *placeLikelihoodList, NSError *error){
+        NSString *name = nil;
+        NSString *address = nil;
         if (error != nil) {
             NSLog(@"Pick Place error %@", [error localizedDescription]);
             return;
         }
-        
-        self.nameLabel.text = @"No current place";
-        self.addressLabel.text = @"";
+
+        name = @"No current place";
+        address = @"";
         
         if (placeLikelihoodList != nil) {
             GMSPlace *place = [[[placeLikelihoodList likelihoods] firstObject] place];
             if (place != nil) {
-                self.nameLabel.text = place.name;
-                self.addressLabel.text = [[place.formattedAddress componentsSeparatedByString:@", "]
+                name = place.name;
+                address = [[place.formattedAddress componentsSeparatedByString:@", "]
                                           componentsJoinedByString:@"\n"];
             }
         }
+        
+        self.nameLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, self.yourCurrentLocationLabel.frame.origin.y+self.yourCurrentLocationLabel.frame.size.height + 20, self.view.frame.size.width, 20)];
+        self.nameLabel.text = name;
+        self.nameLabel.numberOfLines = 0;
+//        [self.nameLabel sizeToFit];
+        self.nameLabel.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:self.nameLabel];
+        
+        self.addressLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, self.nameLabel.frame.origin.y+self.nameLabel.frame.size.height + 20, self.view.frame.size.width, 20)];
+        self.addressLabel.text = address;
+        self.addressLabel.numberOfLines = 0;
+        [self.addressLabel sizeToFit];
+        self.addressLabel.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:self.addressLabel];
+        
     }];
 }
 
